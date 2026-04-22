7e4310a085b53f77559e6c433b250305276778c2
Fatma <123814832+fatmaakpunar@users.noreply.github.com>
2026-04-22 15:40:38 +0300

    Update 22nisan.R

--- 22nisan.R
+++ 22nisan.R
@@ -8,3 +8,30 @@ f <- function(x){
 xd <- seq(-10, 10 lenght.out=100000)
 ad = f(xd)/g(xd)
 abest <- max(ad)=0.01
+
+
+#kabul-ret yöntemi
+#büyük fin olmadığı sürekli dağılımlarda f analitik olarak yazılamaz.
+f<- function(x){
+  x<- 1/sqrt(2*pi)*exp(-(x^2)/2)
+  return(x)
+}
+g <- function(x) dlogis(x, location = 0, scale = 1) #**
+xd <- seq(-10, 10, length.out=100000) #-10,10 kısmı büyütülebilir fakat algoritma yavaşlar
+ad = f(xd)/g(xd)
+abest <- max(ad)=0.01 #1.520271
+
+
+k=0
+kr<-0
+while(k<n){
+  ul<-runif(1)
+  v <- tan(pi*(u1-1/2))
+  
+  u2<-runif(1)
+  kr=kr+1
+  if(u2*)
+  Nx[k] <- v
+}
+#ödev x1, x2,....,xn lambda parametreleri poisson dağılımdan n birimlik örneklem olsun T1=
+  
