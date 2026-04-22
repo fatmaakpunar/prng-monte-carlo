6203c53ad0cd542892d3021c8603d34471d14e2e
Fatma <123814832+fatmaakpunar@users.noreply.github.com>
2026-04-22 14:43:07 +0300

    Create 22nisan.R

---
+++ 22nisan.R
@@ -0,0 +1,10 @@
+#kabul-ret yöntemi
+#büyük fin olmadığı sürekli dağılımlarda f analitik olarak yazılamaz.
+f <- function(x){
+  x<- 1/sqrt(2*pi)*exp(-(x^2)/2)
+  retur(x)
+}
+
+xd <- seq(-10, 10 lenght.out=100000)
+ad = f(xd)/g(xd)
+abest <- max(ad)=0.01
